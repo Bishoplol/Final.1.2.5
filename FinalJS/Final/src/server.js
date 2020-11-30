3e2fd5367854176d5cd34ebf209c4d0b9b0b6dd8
Bishoplol <74049570+Bishoplol@users.noreply.github.com>
2020-11-30 14:54:03 +0300

    Add files via upload

---
+++ FinalJS/Final/src/server.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const fs =require('fs')
+const port = 3000
+
+const server = http.createServer(function(req,res) {
+    res.writeHead(200,{'Content-Type': 'text/html'})
+    fs.readFile('index.html' , function(error,data) {
+        if (error){
+            res.writeHead(404)
+            res.write('Error:file NOT FOUND')
+        }else {
+            res.write(data)
+        }
+        res.end()
+    }) 
+
+
+})
+
+
+server.listen(port,function(error){
+    if (error){
+        console.log('WRONG', error)
+    }else{
+        console.log('server is on the port' + port)
+    }
+
+
+})
